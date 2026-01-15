10d7a3097fb8e13aadeeb0887bf8aa272c492a0e
Akashb101 <akashb101@gmail.com>
2026-01-15 13:46:37 +0800

    Add main App component with routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Chat from './pages/Chat'
+import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
